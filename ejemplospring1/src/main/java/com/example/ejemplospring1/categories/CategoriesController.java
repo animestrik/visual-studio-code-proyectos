4ca8112a7b5d0cb425e8661e0ea59d8ff862247a
animestrik <114590686+animestrik@users.noreply.github.com>
2022-11-14 20:13:16 +0100

    finalizado de momento

--- ejemplospring1/src/main/java/com/example/ejemplospring1/categories/CategoriesController.java
+++ ejemplospring1/src/main/java/com/example/ejemplospring1/categories/CategoriesController.java
@@ -2,9 +2,16 @@ package com.example.ejemplospring1.categories;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -21,7 +28,28 @@ public class CategoriesController {
     }
 
     @GetMapping("/{id}")
-    public Category getCategory(@PathVariable int id) {
-        return catService.getCategory(id);
+    public ResponseEntity<Category> getCategory(@PathVariable int id) {
+        Category c = catService.getCategory(id);
+        if(c == null) {
+            return ResponseEntity.notFound().build();
+        } 
+        return ResponseEntity.ok(c);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Category insertCategory(@RequestBody Category c) {
+        return catService.insert(c);
+    }
+
+    @PutMapping("/{id}")
+    public Category updateCategory(@RequestBody Category c, @PathVariable int id) {
+        return catService.update(c, id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCategory(@PathVariable int id) {
+        catService.delete(id);
     }
 }
--- ejemplospring1/src/main/java/com/example/ejemplospring1/categories/CategoriesService.java
+++ ejemplospring1/src/main/java/com/example/ejemplospring1/categories/CategoriesService.java
@@ -3,7 +3,6 @@ package com.example.ejemplospring1.categories;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
-
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -18,5 +17,19 @@ public class CategoriesService {
 
     public Category getCategory(int id) {
         return catRepository.findById(id).orElse(null);
+    }
+
+    public Category insert(Category c) {
+        return catRepository.save(c);
+    }
+
+    public Category update(Category c, int id) {
+        c.setId(id); // Al tener una id hace un update en lugar de un insert
+        return catRepository.save(c);
+    }
+
+    
+    public void delete(int id) {
+        catRepository.deleteById(id);
     }
 }
